a0db378c57acc7eb63a6cea335ca7a94f5421945
Sang Saephan <sangsaephan510@yahoo.com>
2017-08-22 00:43:12 -0700

    Set up Node project skeleton and CRUD

---
+++ src/middleware/index.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+export default({ config, db}) => {
+  let api = Router();
+  // Add middleware
+
+  return api;
+}
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import config from '../config';
+import middleware from '../middleware';
+import initializeDb from '../db';
+import restaurant from '../controller/restaurant';
+
+let router = express();
+
+// Connect to db
+initializeDb(db => {
+  //Internal middleware
+  router.use(middleware({ config, db}));
+
+  // API routes v1 (/v1)
+  router.use('/restaurant', restaurant({ config, db }));
+});
+
+export default router
